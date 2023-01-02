0241de1126dee41ce3df04057cd022fdd3ef29a3
tamaracosta <tamarasantosc@gmail.com>
2023-01-02 15:28:07 +0100

    chamando componente Banner na pagina inicial

--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -1,11 +1,19 @@
+import Banner from "../../componentes/Banner";
 import Cabecalho from "../../componentes/Cabecalho";
 import Menu from "../../componentes/Menu";
+import styles from "./PaginaInicial.module.scss"
 
 export default function PaginaInicial(){
   return (
     <>
       <Cabecalho />
-      <Menu />
+      <main>
+        <section className={styles.principal}>
+          <Menu />
+          <Banner />
+        </section>        
+      </main>
+      
     </>
    
   )
