9ca75a665eca52eaecb1caf7ff5a2bd692b1576f
tamaracosta <tamarasantosc@gmail.com>
2023-01-02 13:52:04 +0100

    criando componente Cabecalho

---
+++ src/componentes/Cabecalho/index.jsx
@@ -0,0 +1,18 @@
+import logo from "./logo.png";
+import search from "./search.png";
+import styles from "./Cabecalho.module.scss";
+
+export default function Cabecalho(){
+return (
+  <header className={styles.cabecalho}>
+    <img src={logo} alt="logo alura" />
+    <div className={styles.cabecalho__container}>
+      <input 
+      className={styles.cabecalho__input}
+      type="text"
+      placeholder="O que você procura" />
+      <img src={search} alt="lupa"/>
+    </div>
+  </header>
+)
+}
