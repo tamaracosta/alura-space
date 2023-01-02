e28a724956e1c720adff151f1a3e0423721ed2a9
tamaracosta <tamarasantosc@gmail.com>
2023-01-02 21:25:33 +0100

    componente Rodape

---
+++ src/componentes/Rodape/index.jsx
@@ -0,0 +1,35 @@
+import styles from "./Rodape.module.scss";
+import facebook from "./facebook.svg";
+import instagram from "./instagram.svg";
+import twitter from "./twitter.svg";
+
+export default function Rodape() {
+  return (
+    <footer className={styles.rodape}>
+      <div className={styles.rodape__icones}>
+        <a
+          href="https://www.facebook.com/AluraCursosOnline/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={facebook} alt="ícone do facebook" />
+        </a>
+        <a
+          href="https://twitter.com/aluraonline?lang=en"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={twitter} alt="ícone do twitter" />
+        </a>
+        <a
+          href="https://www.instagram.com/aluraonline/?hl=en"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={instagram} alt="ícone do instagram" />
+        </a>
+      </div>
+      <p>Desenvolvido por Alura</p>
+    </footer>
+  )
+}
